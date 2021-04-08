63df8688b8336dddcb50f75ebd8453ef7b682aec
cojoceacristian <Techdeck01>
2021-04-08 20:25:18 +0300

    Included a test message

--- Curs_11.py
+++ Curs_11.py
@@ -1,6 +1,10 @@
 def suma(a,b):
-    vaSuma = a+b
+    varSuma = a+b
 #trebuia sa existe un return aici
 var1 = 5
 var2 = 2
 print(suma(var1,var2))
+
+print("Test")
+
+#pentru fiecare modificare da commit nu uita
